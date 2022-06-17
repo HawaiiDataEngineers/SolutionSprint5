aee2313cddfe65edbd2af3c3f1dc63fe5dd41ba4
HawaiiDataEngineers <97312034+HawaiiDataEngineers@users.noreply.github.com>
2022-06-17 21:15:15 +0100

    Update date_casting.sql

--- sqlstatement/date_casting.sql
+++ sqlstatement/date_casting.sql
@@ -1,3 +1,4 @@
+-- selecionar apenas a amostra de um ano das vendas e selecionar colunas 
 SELECT date(date) as date, city, category_name, item_description, bottles_sold 
 FROM `fiap-challenge-5.liquor_Sales.sales` 
 WHERE date BETWEEN '2020-01-01' AND '2021-01-01';

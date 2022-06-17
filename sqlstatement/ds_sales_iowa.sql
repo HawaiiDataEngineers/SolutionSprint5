ed8308de5eb041af021ada2e3e33a0a08c9bc215
HawaiiDataEngineers <97312034+HawaiiDataEngineers@users.noreply.github.com>
2022-06-17 21:07:40 +0100

    Create ds_sales_iowa.sql

---
+++ sqlstatement/ds_sales_iowa.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM `fiap-challenge-5.resultado_final.ds_sales`
+WHERE city = 'Iowa City' AND category_name ='Straight Rye Whiskies';
+
+SELECT count(*)
+FROM `fiap-challenge-5.resultado_final.ds_sales`
+WHERE city = 'Iowa City' AND category_name ='Straight Rye Whiskies';
